f98025c345badd4943e257b2b859c8f16014547b
alireza85as <79114023+alireza85as@users.noreply.github.com>
2025-08-22 00:43:59 +0330

    Delete one directory

--- easydate/__init__.py
+++
@@ -1,2 +0,0 @@
-from .core import EasyDate
-from .exceptions import InvalidDateFormat
--- easydate/exceptions.py
+++
@@ -1,3 +0,0 @@
-class InvalidDateFormat(Exception):
-    """Raised when the date format is invalid"""
-    pass
